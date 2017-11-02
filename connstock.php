bbe5e139988df76a6e2e5faa8fb500867430ab23
Jacky <junhao8651@gmail.com>
2017-11-02 21:33:58 +0800

    json was turned off

--- connstock.php
+++ connstock.php
@@ -1,124 +1,124 @@
 <?php
-session_start();
-$link = mysql_connect("0.tcp.ngrok.io:12714","root","ncutim")or die ("conn fail");
-mysql_select_db('onmarket',$link) or die ("DB fail");
-mysql_query("SET NAMES 'utf-8'");
-mysql_query("SET CHARACTER_SET_CLIENT=utf-8");
-mysql_query("SET CHARACTER_SET_RESULTS=utf8");
-#for($q = 1000;$q < 9000;$q++){
-$sqllimit="SELECT * FROM `1101`; ";
-$result = mysql_query($sqllimit) or die ('mySQL query error');
-while($row = mysql_fetch_array($result, MYSQL_NUM)){
-    for($q=1;$q<9;$q++) {
-        echo $row[$q];
-    }
-    }
-?>
+//session_start();
+//$link = mysql_connect("0.tcp.ngrok.io:12714","root","ncutim")or die ("conn fail");
+//mysql_select_db('onmarket',$link) or die ("DB fail");
+//mysql_query("SET NAMES 'utf-8'");
+//mysql_query("SET CHARACTER_SET_CLIENT=utf-8");
+//mysql_query("SET CHARACTER_SET_RESULTS=utf8");
+//$sqllimit="SELECT * FROM `1540`; ";
+//$result = mysql_query($sqllimit) or die ('mySQL query error');
+//while($row = mysql_fetch_array($result)) {
+//    echo $row['ID'];
+//}
+//?>
+
+<!DOCTYPE html>
 <html>
 <head>
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-    <script src="https://code.highcharts.com/stock/highstock.js"></script>
-    <script src="https://code.highcharts.com/stock/modules/drag-panes.js"></script>
-    <script src="https://code.highcharts.com/stock/modules/exporting.js"></script>
+    <script src="jquery.js"></script>
+    <script src="highcharts.js"></script>
+    <link href="chartsss.css" type="text/css" rel="stylesheet">
+    <div id="Header"></div>
+    <div id="Sidebar"></div>
+    <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto">
+        <script>
+            function opt1() {
+                var opt = document.type.selectedIndex;
+                if (opt.value==1){
+                    alert(a);
+                }
+            }
+            var day = ['2017/10/14','2017/10/15','2017/10/16','2017/10/17'];
+            var view = [300,150,100,200];
+            var eqw = [30,15,10,20];
+            Highcharts.chart('container', {
+                chart: {
+                    type: 'area'
+                },
+                title: {
+                    text: '簡單每日人氣表'
+                },
+                subtitle: {
+                    text: 'Source: never-nop.com'
+                },
+                xAxis: {
+                    maxpadding: 0.6,
+                    categories:day,
+                    tickmarkPlacement: 'on',
+                    title: {
+                        enabled: false
+                    }
+                },
+                yAxis: [{
+                    title: {
+                        text: 'Voltage'
+                    },
+                    labels: {
+                        formatter: function () {
+                            return this.value
+                        },
+                        showFirstLabel: true
+                    }
+                },{
+                    itle: {
+                        text: null
+                    },
+                    labels: {
+                        formatter: function () {
+                            return this.value
+                        },
+                        showFirstLabel: false
+                    }
+
+                }],
+                tooltip: {
+                    split: true,
+                    valueSuffix: ' V'
+                },
+                plotOptions: {
+                    area: {
+                        stacking: 'normal',
+                        lineColor: '#666666',
+                        lineWidth: 1,
+                        marker: {
+                            lineWidth: 1,
+                            lineColor: '#666666'
+                        }
+                    }
+                },
+                series: [{
+                    name: 'voltage',
+                    data: view
+                },{
+                    name: '987',
+                    data: eqw
+                }
+                ]
+            });
+        </script>
+    </div>
+    <div style='clear:both;'></div>
+    <div id="Footer">Footer 欄位</div>
+    <!--<link rel=stylesheet type="text/css" href="cha.css">-->
+    <meta charset="utf-8">
+    <title>測試</title>
+
 <body>
 
 
-
-<div id="container" style="height: 400px; min-width: 310px"></div>
-<script>
-    function (data) {
-        // split the data set into price and volume
-        var price = [],
-            volume = [],
-            dataLength = data.length,
-            // set the allowed units for data grouping
-            groupingUnits = [[
-                'week',                         // unit name
-                [1]                             // allowed multiples
-            ], [
-                'month',
-                [1, 2, 3, 4, 6]
-            ]],
-
-            i = 0;
-
-        for (i; i < dataLength; i += 1) {
-            price.push([
-                data[i][0], // the date
-                data[i][1], // open
-                data[i][2], // high
-                data[i][3], // low
-                data[i][4] // close
-            ]);
-
-            volume.push([
-                data[i][0], // the date
-                data[i][5] // the volume
-            ]);
-        }
+<select name="type" class="opts" id="type" onchange="opt1()">
+    <option selected value="1">折線圖</option>
+    <option value="2">圓餅圖</option>
+    <option value="3">柱狀圖</option>
+</select>
+<select id="data"name="data" class="opts" >
+    <option  value="4">電壓</option>
+    <option value="5">電流</option>
+    <option value="6">溫度</option>
+</select>
+<button type="button" onclick="displayResult()">show!</button>
 
 
-        // create the chart
-        Highcharts.stockChart('container', {
-
-            rangeSelector: {
-                selected: 1
-            },
-
-            title: {
-                text: '2330台積電'
-            },
-
-            yAxis: [{
-                labels: {
-                    align: 'right',
-                    x: -3
-                },
-                title: {
-                    text: '2330'
-                },
-                height: '60%',
-                lineWidth: 2,
-                resize: {
-                    enabled: true
-                }
-            }, {
-                labels: {
-                    align: 'right',
-                    x: -3
-                },
-                title: {
-                    text: 'Volume'
-                },
-                top: '65%',
-                height: '35%',
-                offset: 0,
-                lineWidth: 2
-            }],
-
-            tooltip: {
-                split: true
-            },
-
-            series: [{
-                type: 'candlestick',
-                name: '台積電',
-                data: price,
-                dataGrouping: {
-                    units: groupingUnits
-                }
-            }, {
-                type: 'column',
-                name: 'Volume',
-                data: volume,
-                yAxis: 1,
-                dataGrouping: {
-                    units: groupingUnits
-                }
-            }]
-        });
-    });
-</script>
 </body>
 </head>
 </html>
